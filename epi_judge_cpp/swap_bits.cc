43c63cd6d0a8e79d0caaf7d9702eb23989189e8c
jonic.mecija <jonic.mecija@yahoo.com>
2022-11-13 14:27:13 -0800

    swap bits

--- epi_judge_cpp/swap_bits.cc
+++ epi_judge_cpp/swap_bits.cc
@@ -1,8 +1,28 @@
 #include "test_framework/generic_test.h"
+#include <iostream>
+
 long long SwapBits(long long x, int i, int j) {
   // TODO - you fill in here.
-  return 0;
+  int temp_i = 0;
+  int temp_j = 0;
+
+  temp_i = (x >> i) & 1;
+  temp_j = (x >> j) & 1;
+
+  if(temp_i != temp_j){
+    unsigned long long bit_mask = (1L << i) | (1L << j);
+    x ^= bit_mask;
+  } 
+  
+  return x;
 }
+
+//   i    j
+// 0101_0110
+// 0010_0010 ^ <-
+
+// result
+// 0111_0100
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
