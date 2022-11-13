336f17e42b6d079c02ce02f6008b390f6406ac29
jonic.mecija <jonic.mecija@yahoo.com>
2022-11-13 14:09:11 -0800

    parity of a word

--- epi_judge_cpp/parity.cc
+++ epi_judge_cpp/parity.cc
@@ -1,7 +1,25 @@
 #include "test_framework/generic_test.h"
 short Parity(unsigned long long x) {
   // TODO - you fill in here.
-  return 0;
+
+  // brute force solution
+
+  uint8_t result = 0;
+
+  // while(x){
+  //   result ^= (x & 1);
+  //   x >>= 1;
+  // }
+  // return result;
+
+  // improved run time by erasing last bit set to one
+
+  while(x){
+    result ^= 1;
+    x &= (x-1); // gets rid of last bit set to 1
+  }
+
+  return result;
 }
 
 int main(int argc, char* argv[]) {
@@ -10,3 +28,4 @@ int main(int argc, char* argv[]) {
   return GenericTestMain(args, "parity.cc", "parity.tsv", &Parity,
                          DefaultComparator{}, param_names);
 }
+// g++ -pthread -std=c++14 -O3 -o count_bits count_bits.cc
