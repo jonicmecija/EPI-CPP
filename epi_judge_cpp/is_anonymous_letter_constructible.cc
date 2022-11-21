fca134859386bbb28ea2b87a10ff7ea35a4d7d2a
jonic.mecija <jonic.mecija@yahoo.com>
2022-11-21 13:45:24 -0800

    anon optimal

--- epi_judge_cpp/is_anonymous_letter_constructible.cc
+++ epi_judge_cpp/is_anonymous_letter_constructible.cc
@@ -8,41 +8,30 @@ bool IsLetterConstructibleFromMagazine(const string& letter_text,
                                        const string& magazine_text) {
   // TODO - you fill in here.
   std::unordered_map<char, int> letterHash;
-  std::unordered_map<char, int> magHash;
 
+  // store char occurances of letter text
   for (char c : letter_text){
-    if(letterHash.find(c) == letterHash.end()){
-      letterHash.insert({c, 1});
-    }
-    else{
-      letterHash.at(c)++;
-    }
+    letterHash[c]++;
   }
+
   for (char c : magazine_text){
-    if(magHash.find(c) == magHash.end()){
-      magHash.insert({c, 1});
-    }
-    else{
-      magHash.at(c)++;
-    }
-  }
-
-  //now that occurrances of both text are stored, compare
-  // if(letterHash.size() != magHash.size()){
-  //   return false;
-  // }
-
-  // every char in letter must be equal or less than that of occurances of that char in magazine
-
-  for( auto pair : letterHash){
-    if(magHash.find(pair.first) != magHash.end() && pair.second <= magHash[pair.first]){
-      continue;
-    }
-    else{
-      return false;
+    if(letterHash.find(c) == letterHash.end()){
+      
+    }else{
+      if(letterHash.at(c)){
+        letterHash[c]--;
+        if(letterHash[c] == 0){
+          letterHash.erase(c);
+        }
+      }
+        
+      if(letterHash.empty()){
+        break;
+      }
     }
   }
-  return true;
+
+  return letterHash.empty();
 }
 
 int main(int argc, char* argv[]) {
