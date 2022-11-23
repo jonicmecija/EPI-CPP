d28e90de63ef03038958804a91c35fa77b0e424d
jonic.mecija <jonic.mecija@yahoo.com>
2022-11-22 20:55:29 -0800

    int array plus one

--- epi_judge_cpp/int_as_array_increment.cc
+++ epi_judge_cpp/int_as_array_increment.cc
@@ -3,8 +3,29 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 vector<int> PlusOne(vector<int> A) {
-  // TODO - you fill in here.
-  return {};
+  int ptr = A.size() - 1;
+
+  if(A[ptr] != 9){
+    A.back()++;
+  }else{
+    while(A[ptr] == 9){
+      A[ptr] = 0;
+
+      if(ptr > 0){
+        ptr--;
+        if(A[ptr] != 9){
+          A[ptr]++;
+          break;
+        }
+        continue;
+      }
+      else if(ptr == 0){
+        A.insert(A.begin(), 1);
+        ptr--;
+      }
+    }
+  }
+  return A;
 }
 
 int main(int argc, char* argv[]) {
